89c8cc07252a93f61c86f6c3123868279cfdab36
Aditya Jain <adityajain907935@gmail.com>
2025-10-05 10:17:36 +0530

    Specify server IP address in app.listen

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ connectDB()
       throw error;
     });
     
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8000, "3.109.157.61" , () => {
         console.log(`Server is running at port : ${process.env.PORT}`);
     })
 })
