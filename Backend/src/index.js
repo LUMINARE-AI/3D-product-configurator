b6183e6e065b9039d6a46314e0ccb775a34bb02f
Aditya Jain <adityajain907935@gmail.com>
2025-10-08 01:39:27 +0530

    Refactor index.js for improved code formatting and consistency

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,27 +1,25 @@
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
-import {app} from "./app.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './.env'
-}) 
+  path: "./.env",
+});
 
 connectDB()
-.then(() => {
+  .then(() => {
     app.on("error", (error) => {
       console.error("ERROR:", error);
       throw error;
     });
-    
-    app.listen(process.env.PORT || 8000, "3.109.157.61" , () => {
-        console.log(`Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
+
+    app.listen(process.env.PORT || 8000, "0.0.0.0", () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-}) 
-
-
+  });
 
 /*
 import express from "express";
